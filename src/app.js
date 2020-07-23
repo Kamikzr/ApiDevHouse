546712711148eda229b1de49a1047edc818f3ff0
Marcelo Yida <62110375+Kamikzr@users.noreply.github.com>
2020-07-23 20:12:10 -0300

    atualização banco de dados

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const routes = require('./routes');
+const mysql = require('mysql');
+const dotenv = require('dotenv');
 
 class App{
     constructor(){
         this.server = express();
 
+        const db = mysql.createConnection({
+            host: process.env.DATABASE_HOST,
+            user: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE
+        });
+
+        db.connect( (err) => {
+            if(err) {
+                console.log(err.sqlMessage)
+            } else {
+                console.log("MYSQL Connected...")
+            }
+        });
+ 
         this.middlewares();
         this.routes();
     }
